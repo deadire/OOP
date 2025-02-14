af1cc5440ce25e4448365f2d78657d2b3d80323b
deadire <adeelrumaan123@gmail.com>
2025-02-14 12:35:40 +0500

    Copy
    copied the code

--- inputdata.java
+++ inputdata.java
@@ -8,3 +8,14 @@ public class inputdata {
         System.out.printf("Your name is %s\n", name);
     }
 }
+
+import java.util.Scanner;
+public class inputdata {
+    public static void main(String args[]){
+        Scanner sc1 = new Scanner(System.in);
+        String name1 = "";
+        System.out.println("What is your name?");
+        name1 = sc.next();
+        System.out.printf("Your name is %s\n", name1);
+    }
+}
