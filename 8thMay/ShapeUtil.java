93acd2f8c0b3e74037fe26b9898bdb94a109f2dd
deadire <adeelrumaan123@gmail.com>
2025-05-08 17:39:29 +0500

    Updated Working

--- 8thMay/ShapeUtil.java
+++ 8thMay/ShapeUtil.java
@@ -12,11 +12,21 @@ public class ShapeUtil {
         target.setLength(length + target.getLength());
     }
 
-    public boolean Intersection (Rectangle rectangle1, Rectangle rectangle2){
-        if (rectangle1.getTopLeftCorner().getX() > rectangle2.getTopLeftCorner().getX() && rectangle1.getTopLeftCorner().getX() < rectangle2.getTopLeftCorner().getX() + rectangle2.getLength()){
-            return true;
-        } else {
+    public boolean intersects(Rectangle r1, Rectangle r2) {
+        double r1Left = r1.getTopLeftCorner().getX();
+        double r1Right = r1Left + r1.getWidth();
+        double r1Top = r1.getTopLeftCorner().getY();
+        double r1Bottom = r1Top - r1.getLength();
+
+        double r2Left = r2.getTopLeftCorner().getX();
+        double r2Right = r2Left + r2.getWidth();
+        double r2Top = r2.getTopLeftCorner().getY();
+        double r2Bottom = r2Top - r2.getLength();
+
+        if (r1Left >= r2Right || r1Right <= r2Left || r1Top <= r2Bottom || r1Bottom >= r2Top) {
             return false;
         }
+        return true;
     }
+
 }
--- 8thMay/main.java
+++ 8thMay/main.java
@@ -3,42 +3,50 @@ public class main {
         Shape shape1;
         Shape2D shape2;
         Shape3D shape3;
-        Circle circle1 = new Circle("circle1",24);
+
+        Circle circle1 = new Circle("circle1", 24);
         circle1.setRadius(2);
         System.out.println("Area of Circle is: " + circle1.Area());
-        Rectangle rectangle1 = new Rectangle("rectangle1",24,24);
+
+        Rectangle rectangle1 = new Rectangle("rectangle1", 24, 24);
         rectangle1.setLength(2);
         rectangle1.setWidth(2);
-        rectangle1.setTopLeftCorner(new Point(10,10));
-        System.out.println("Area of Rectangle is: " + rectangle1.Area());
-        Rectangle rectangle2 = new Rectangle("rectangle2",24,24);
-        rectangle1.setLength(4);
-        rectangle1.setWidth(4);
-        rectangle1.setTopLeftCorner(new Point(20,20));
-        System.out.println("Area of Rectangle is: " + rectangle2.Area());
+        rectangle1.setTopLeftCorner(new Point(10, 10));
+        System.out.println("Area of Rectangle 1 is: " + rectangle1.Area());
+
+        Rectangle rectangle2 = new Rectangle("rectangle2", 24, 24);
+        rectangle2.setLength(4);
+        rectangle2.setWidth(4);
+        rectangle2.setTopLeftCorner(new Point(20, 20));
+        System.out.println("Area of Rectangle 2 is: " + rectangle2.Area());
+        
+        if (intersects(rectangle1, rectangle2)) {
+            System.out.println("Rectangles intersect.");
+        } else {
+            System.out.println("Rectangles do NOT intersect.");
+        }
+
         Shape[] shapes = new Shape[20];
-        shapes[0] = circle1;
-        shapes[1] = rectangle1;
-        shapes[2] = circle1;
-        shapes[3] = rectangle1;
-        shapes[4] = circle1;
-        shapes[5] = rectangle1;
-        shapes[6] = circle1;
-        shapes[7] = rectangle1;
-        shapes[8] = circle1;
-        shapes[9] = rectangle1;
-        shapes[10] = circle1;
-        shapes[11] = rectangle1;
-        shapes[12] = circle1;
-        shapes[13] = rectangle1;
-        shapes[14] = circle1;
-        shapes[15] = rectangle1;
-        shapes[16] = circle1;
-        shapes[17] = rectangle1;
-        shapes[18] = circle1;
-        shapes[19] = rectangle1;
+        for (int i = 0; i < 20; i++) {
+            shapes[i] = (i % 2 == 0) ? circle1 : rectangle1;
+        }
+
         ShapeUtil shapeUtil = new ShapeUtil();
         System.out.println("Shapes are:");
         shapeUtil.displayShapes(shapes);
+    }
+
+    public static boolean intersects(Rectangle r1, Rectangle r2) {
+        double r1Left = r1.getTopLeftCorner().getX();
+        double r1Right = r1Left + r1.getWidth();
+        double r1Top = r1.getTopLeftCorner().getY();
+        double r1Bottom = r1Top + r1.getLength();
+
+        double r2Left = r2.getTopLeftCorner().getX();
+        double r2Right = r2Left + r2.getWidth();
+        double r2Top = r2.getTopLeftCorner().getY();
+        double r2Bottom = r2Top + r2.getLength();
+
+        return !(r1Left >= r2Right || r1Right <= r2Left || r1Top >= r2Bottom || r1Bottom <= r2Top);
     }
 }
